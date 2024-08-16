7d9600bc39b306dbd0e20521c0e6cc78e336b442
Cauê Carneiro <160974341+cauecarneiro@users.noreply.github.com>
2024-08-16 16:35:57 -0300

    Update and rename ano bisexto.c to Ano-Bissexto.c

--- Ano-Bissexto.c
+++ Ano-Bissexto.c
@@ -7,8 +7,8 @@ void main() {
     printf("Digite um ano qualquer: ");
     scanf("%i", &ano);
     if(ano%4==0 && ano%100!=0 || ano%400==0) {
-            printf("O ano de %i È bissexto!", ano);
+            printf("O ano de %i √© bissexto!", ano);
         }else{
-            printf("O ano de %i n„o È bissexto!", ano);
+            printf("O ano de %i n√£o √© bissexto!", ano);
         }
 }
