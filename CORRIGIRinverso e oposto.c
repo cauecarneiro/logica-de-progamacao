28f2ca5aa5e7fb3c61b2063d1522ce8b881797bd
Cauê Carneiro <160974341+cauecarneiro@users.noreply.github.com>
2024-08-16 17:43:12 -0300

    Update and rename inverso e oposto.c to CORRIGIRinverso e oposto.c

--- CORRIGIRinverso e oposto.c
+++ CORRIGIRinverso e oposto.c
@@ -3,15 +3,15 @@
 void main () {
     setlocale(0,"Portuguese");
     float n;
-    printf("Digite um número: ");
+    printf("Digite um nÃºmero: ");
     scanf("%f", &n);
     if (n>0) {
         float i = 1/n;
-        printf("O inverso de %.0f é igual a %.2f", n, i);
+        printf("O inverso de %.0f Ã© igual a %.2f", n, i);
 
     }else{
         float o = -(n);
-        printf("O oposto de %.0f é igual a %1.f", n, o);
+        printf("O oposto de %.0f Ã© igual a %1.f", n, o);
 
     }
 
