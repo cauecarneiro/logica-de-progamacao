3c772671d917ee022b14345378494fcfa072fb82
Cauê Carneiro <160974341+cauecarneiro@users.noreply.github.com>
2024-08-16 16:42:38 -0300

    Update and rename desconto.c to Desconto2.c

--- Desconto2.c
+++ Desconto2.c
@@ -6,14 +6,14 @@ float v;
     printf("Qual foi o valor total das compras? R$");
     scanf("%f", &v);
     printf("---------------------------------------");
-    printf("\nVocê comprou R$%.2f na nossa loja", v);
+    printf("\nVocÃª comprou R$%.2f na nossa loja", v);
 
     if (v>500) {
-        printf("\n========== ATENÇÃO ==========");
+        printf("\n========== ATENÃ‡ÃƒO ==========");
         float d = v*0.90;
         float vn = v-d;
-        printf("\nPor fazer mais de R$500,00 em compras, você vai receber R$%2.f de desconto", vn);
-        printf("\nO valor a ser pago é R$%2.f! Volte sempre!", d);
+        printf("\nPor fazer mais de R$500,00 em compras, vocÃª vai receber R$%2.f de desconto", vn);
+        printf("\nO valor a ser pago Ã© R$%2.f! Volte sempre!", d);
         printf("\n---------------------------------------");
 }
     printf("\nVolte sempre!");
