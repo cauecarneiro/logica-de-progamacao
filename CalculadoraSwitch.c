61cd0ea534ce1f835ee3d41c97685ff794e7d33d
Cauê Carneiro <160974341+cauecarneiro@users.noreply.github.com>
2024-08-23 12:44:24 -0300

    Update CalculadoraSwitch.c

--- CalculadoraSwitch.c
+++ CalculadoraSwitch.c
@@ -5,53 +5,54 @@
 int main(int argc, char *argv[]){
     setlocale(0,"Portuguese");
     float n1, n2, res;
-    int op;
+    char op;
     printf("==== CALCULADORA ====");
-    printf("\n\nDigite 1 para soma\nDigite 2 para subtrair\nDigite 3 para multiplicar\nDigite 4 para divis„o\n");
-    scanf("%i", &op);
-    
+    printf("\n\nDigite + para soma\nDigite - para subtrair\nDigite * para multiplicar\nDigite / para divis√£o\n\nDigite aqui: ");
+    scanf("%c", &op);
+
 
     switch(op) {
-    	
-    	case 1:
-    		printf("Digite o primeiro valor: ");
+
+    	case '+':
+    		printf("\nDigite o primeiro valor: ");
     		scanf("%f", &n1);
     		printf("Digite o segundo valor: ");
     		scanf("%f", &n2);
         res = n1 + n2;
-        printf("Soma: %.1f", res);
+        printf("\nSoma: %.1f", res);
         break;
 
-    	case 2:
-    		printf("Digite o primeiro valor: ");
+    	case '-':
+    		printf("\nDigite o primeiro valor: ");
     		scanf("%f", &n1);
     		printf("Digite o segundo valor: ");
     		scanf("%f", &n2);
         res = n1 - n2;
-        printf("SubtraÁ„o: %.1f", res);
+        printf("\nSubtra√ß√£o: %.1f", res);
 		break;
 
-    	case 3:
-    		printf("Digite o primeiro valor: ");
+    	case '*':
+    		printf("\nDigite o primeiro valor: ");
     		scanf("%f", &n1);
     		printf("Digite o segundo valor: ");
     		scanf("%f", &n2);
         res = n1 * n2;
-        printf("MultiplicaÁ„o: %1.f", res);
+        printf("\nMultiplica√ß√£o: %1.f", res);
     	break;
-	
-    	case 4:
-    		printf("Digite o primeiro valor: ");
+
+    	case '/':
+    		printf("\nDigite o primeiro valor: ");
     		scanf("%f", &n1);
     		printf("Digite o segundo valor: ");
     		scanf("%f", &n2);
         res = n1 / n2;
-        printf("Divis„o: %1.f", res);
+        printf("\nDivis√£o: %1.f", res);
         break;
-        
-        default: printf("Digite um n˙mero entre 1 e 4");
+
+        default: printf("\nOpera√ß√£o n√£o encontrada, Digite + - * / \n\n");
     }
 
 
     return 0;
 }
+
