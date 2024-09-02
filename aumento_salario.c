6c958623f484241e80254f0a13f77b4b16665749
Cauê Carneiro <160974341+cauecarneiro@users.noreply.github.com>
2024-09-02 15:48:56 -0300

    Update and rename Aumento-Salario.c to aumento_salario.c

--- aumento_salario.c
+++ aumento_salario.c
@@ -5,14 +5,14 @@
 int main (int argc, char *argv[]) {
     setlocale(0,"Portuguese");
     float sal, per, aum, nsal;
-    printf("Insira o sal·rio: ");
+    printf("Insira o sal√°rio: ");
     scanf("%f", &sal);
     printf("Insira o percentual de aumento: ");
     scanf("%f", &per);
     aum= sal*per/100;
     printf("Valor do aumento: %.0f", aum);
     nsal=sal+aum;
-    printf("\nNovo sal·rio: %.0f", nsal);
+    printf("\nNovo sal√°rio: %.0f", nsal);
 
     return 0;
 }
