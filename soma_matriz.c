4c983dbfe777cc404f3110d92462b22edc335ea6
Cauê Carneiro <160974341+cauecarneiro@users.noreply.github.com>
2024-09-19 14:06:44 -0300

    Add files via upload

---
+++ soma_matriz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(0,"Portuguese");
+	
+	int mat[5][7], i, j, soma;
+	
+	for(i = 0; i < 7; i++){
+		for(j = 0; j<5; j++){
+			printf("Digite um valor para a matriz [%d][%d]: ", j, i);
+			scanf("%d", &mat[j][i]);
+		}
+	}
+	
+	for(i = 0; i < 7; i++){
+		for(j = 0; j<5; j++){
+			soma += mat[j][i];
+		}
+	}
+	
+	printf("Soma: %d", soma);
+}
